0274ffac804020649e90c11d8c0078f0263474ed
José Santos <jose_renato_n_s@hotmail.com>
2026-05-18 19:54:35 -0400

    Adição de novos registros de produtos e fornecedores

--- sql/inserts.sql
+++ sql/inserts.sql
@@ -8,3 +8,14 @@ INSERT INTO entradas_estoque
 VALUES (1, 1, 50, SYSDATE);
 
 COMMIT;
+
+INSERT INTO fornecedores
+VALUES (2, 'Fornecedor B', '(67)98888-8888', 'fornecedorb@email.com');
+
+INSERT INTO produtos
+VALUES (2, 'Feijão', 'Alimento', 12.90, 50, 2);
+
+INSERT INTO produtos
+VALUES (3, 'Macarrão', 'Alimento', 8.50, 15, 1);
+
+COMMIT;

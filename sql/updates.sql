2261efb421758c6318f90411458cbc4d52d876b5
José Santos <jose_renato_n_s@hotmail.com>
2026-05-18 19:55:24 -0400

    Adição de comandos UPDATE para produtos

--- sql/updates.sql
+++ sql/updates.sql
@@ -3,3 +3,14 @@ SET quantidade_estoque = 120
 WHERE id_produto = 1;
 
 COMMIT;
+
+UPDATE produtos
+SET preco = 13.50
+WHERE id_produto = 2;
+
+-- Atualização de estoque
+UPDATE produtos
+SET quantidade_estoque = quantidade_estoque + 20
+WHERE id_produto = 3;
+
+COMMIT;
